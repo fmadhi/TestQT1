1c79b5f9ff924fcb66c713d4786141043def0bf4
F. Madhi <fmadhi19@gmail.com>
2018-06-30 10:25:22 -0700

    Added Close and Open bottom to the Ui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,7 +6,10 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+
     ui->setupUi(this);
+    ui->qOpenBottom->setText("Open");
+    ui->qCloseBottom->setText("Close");
     qDebug() <<"This is a test";
 }
 
